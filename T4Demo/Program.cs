a5920f97d0f1f1a1725801f4e4adbbe8becc5a3d
Sealee <sealee02@gmail.com>
2019-01-08 23:44:00 +0800

    获取表和列

--- T4Demo/Program.cs
+++ T4Demo/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Sealee.Util;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,15 @@ namespace T4Demo
     {
         static void Main(string[] args)
         {
-            string a = "";
+            #region 获取表和列
+            string tables = "";
+            string connectionStrng = @"server=PV-X00273458\SQLEXPRESS;uid=sa;pwd=123;database=SchoolDB";
+            List<DbTable> tablesList = DbHelper.GetDbTables(connectionStrng, "SchoolDB", tables);
+            foreach (DbTable item in tablesList)
+            {
+                List<DbColumn> columnList = DbHelper.GetDbColumns(connectionStrng, "SchoolDB", item.TableName);
+            }
+            #endregion
         }
     }
 }
