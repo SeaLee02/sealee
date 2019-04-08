cb5dc28d80ad5dd5b0e8007858c6f2a485918c29
Sealee <sealee02@gmail.com>
2019-04-08 22:26:56 +0800

    修改

---
+++ Sealee.T4Helper/MyConfig.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sealee.T4Helper
+{
+    public class MyConfig
+    {
+        /// <summary>
+        /// 连接字符串
+        /// </summary>
+        public string ConnectionString { get; set; }
+
+        /// <summary>
+        /// 表名
+        /// </summary>
+        public string TableName { get; set; }
+
+        /// <summary>
+        /// 命名空间
+        /// </summary>
+        public string NameSpace { get; set; }          
+
+        /// <summary>
+        /// 是否覆盖文件
+        /// </summary>
+        public bool IsOverrideFile { get; set; }
+
+        /// <summary>
+        /// 版本
+        /// </summary>
+        public string Version { get; set; }     
+    }
+}
--- Sealee.T4Helper/StringUtil.cs
+++ Sealee.T4Helper/StringUtil.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Web.Script.Serialization;
 
 namespace Sealee.T4Helper
 {
@@ -12,6 +14,16 @@ namespace Sealee.T4Helper
         public static string GetUpperF(string str)
         {
             return Thread.CurrentThread.CurrentCulture.TextInfo.ToTitleCase(str);
+        }
+
+        public static MyConfig GetConfig(string filePath=null)
+        {
+            if (File.Exists(filePath))
+            {
+                JavaScriptSerializer javaScriptSerializer = new JavaScriptSerializer();
+                return javaScriptSerializer.Deserialize<MyConfig>(File.ReadAllText(filePath));
+            }
+            return null;
         }
     }
 }
